66e6d056b08569882fa5d871633c57c92248dd2c
Samuel Veiga <sveiga@elpuig.xeill.net>
2022-10-25 15:53:43 +0200

    Activitat 1 - Multiplicacio

---
+++ src/UF2/Main.java
@@ -0,0 +1,24 @@
+package UF2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static final int numVegades = 10;
+
+    public static void main(String[] args) {
+
+        List<Multiplicacio> llistaMultiplicacions = new ArrayList<Multiplicacio>();
+
+        for (int i = 0; i < numVegades; i++) {
+            Multiplicacio calcul = new Multiplicacio((int)(Math.random()*10), (int)(Math.random()*10));
+            llistaMultiplicacions.add(calcul);
+        }
+
+        for (int i = 0; i < llistaMultiplicacions.size(); i++) {
+            System.out.println("El resultat de la " + (i+1) + " operacio es: " + llistaMultiplicacions.get(i).operacioMultiplicacio());
+        }
+
+    }
+}
--- src/UF2/Multiplicacio.java
+++ src/UF2/Multiplicacio.java
@@ -1,4 +1,15 @@
 package UF2;
 
 public class Multiplicacio {
+    private int num1;
+    private int num2;
+
+    public Multiplicacio(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public Integer operacioMultiplicacio() {
+        return num1*num2;
+    }
 }
