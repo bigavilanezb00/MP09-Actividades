baefcc28b7f97277aafe668eb5217561098e72ea
Samuel Veiga <sveiga@elpuig.xeill.net>
2022-10-25 16:10:16 +0200

    Activitat 1 - Temp

--- src/UF2/Activitat1/Main.java
+++ src/UF2/Activitat1/Main.java
@@ -10,14 +10,18 @@ public class Main {
 
         List<Multiplicacio> llistaMultiplicacions = new ArrayList<Multiplicacio>();
 
+        long temp1 = System.currentTimeMillis();
         for (int i = 0; i < numVegades; i++) {
             Multiplicacio calcul = new Multiplicacio((int)(Math.random()*10), (int)(Math.random()*10));
             llistaMultiplicacions.add(calcul);
         }
 
+        long temp2 = System.currentTimeMillis();
         for (int i = 0; i < llistaMultiplicacions.size(); i++) {
             System.out.println("El resultat de la " + (i+1) + " operacio es: " + llistaMultiplicacions.get(i).operacioMultiplicacio());
         }
 
+        System.out.println();
+        System.out.println("Les operacions han trigat " + (temp2-temp1) + " Milisegons!");
     }
 }
